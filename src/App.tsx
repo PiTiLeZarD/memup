9380f6c2be466a9e5e200a8d6ab3ce6b8852a08a
Jonathan Adami <pitilezard@gmail.com>
2022-12-21 09:39:41 +1000

    start with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import React, { useState } from "react";
 import { Mem } from "./Mem";
 import { useStore } from "./store";
 
@@ -8,8 +9,17 @@ export type AppComponent = React.FunctionComponent<AppProps>;
 
 export const App: AppComponent = (): JSX.Element => {
     const mems = useStore(({ mems }) => mems);
+    const [addOpen, setAddOpen] = useState<boolean>(false);
     return (
         <div>
+            <Button onClick={() => setAddOpen(true)}>Add a mem</Button>
+            <Dialog open={addOpen} onClose={() => setAddOpen(false)}>
+                <DialogTitle>Add a mem</DialogTitle>
+                <DialogContent>Test</DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setAddOpen(false)}>Close</Button>
+                </DialogActions>
+            </Dialog>
             {mems.map((mem) => (
                 <Mem data={mem} />
             ))}
