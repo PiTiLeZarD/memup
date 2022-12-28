5105b3a11c200cb19e81631a837d26fd6f4d04dc
Jonathan Adami <pitilezard@gmail.com>
2022-12-28 12:57:08 +1000

    Increase features around LearnPage

--- src/LearnPage.tsx
+++ src/LearnPage.tsx
@@ -3,26 +3,31 @@ import React, { useState } from "react";
 import { Box, Button, ButtonGroup, Divider, Grid, Stack, Typography } from "@mui/material";
 
 import { Furigana } from "./Furigana";
-import { memDeck, useStore } from "./store";
+import { memDeck, MemType, useStore } from "./store";
 
 export type LearnPageProps = {};
 
 export type LearnPageComponent = React.FunctionComponent<LearnPageProps>;
 
 export const LearnPage: LearnPageComponent = (): JSX.Element => {
-    const mems = memDeck(useStore(({ mems }) => mems));
-
+    const [mems, _] = useState<MemType[]>(memDeck(useStore(({ mems }) => mems)));
     const [currentMem, setCurrentMem] = useState<number>(0);
+    const [showMe, setShowMe] = useState<boolean>(false);
 
     if (mems.length == 0) {
         return <Typography variant="h4">You're all caught up!</Typography>;
     }
 
-    const { mem, furigana, hint } = mems[currentMem];
+    const nextMem = () => {
+        setShowMe(false);
+        setCurrentMem(currentMem + 1);
+    };
+
+    const { mem, furigana, description, hint } = mems[currentMem];
     return (
         <Grid container>
-            <Grid item xs={2}></Grid>
-            <Grid item xs={8}>
+            <Grid item xs={0} md={2}></Grid>
+            <Grid item xs={12} md={8}>
                 <Box sx={{ padding: "2em" }}>
                     <Stack spacing={6} sx={{ textAlign: "center" }}>
                         <Typography variant="h5">
@@ -32,18 +37,35 @@ export const LearnPage: LearnPageComponent = (): JSX.Element => {
                             {(furigana || []).length ? <Furigana furigana={furigana as string[]}>{mem}</Furigana> : mem}
                         </Typography>
                         <Divider />
+                        {showMe && (
+                            <>
+                                <Typography>{description}</Typography>
+                                <Divider />
+                            </>
+                        )}
                         <Box sx={{ textAlign: "center" }}>
                             <ButtonGroup variant="contained" size="large">
-                                <Button color="warning">No Idea</Button>
-                                <Button color="primary">Show me</Button>
-                                <Button disabled={!hint}>Hint!</Button>
-                                <Button color="success">I know</Button>
+                                {showMe && <Button onClick={nextMem}>Next</Button>}
+                                {!showMe && (
+                                    <>
+                                        <Button color="warning" onClick={() => setShowMe(true)}>
+                                            No Idea
+                                        </Button>
+                                        <Button color="primary" onClick={() => setShowMe(true)}>
+                                            Show me
+                                        </Button>
+                                        <Button disabled={!hint}>Hint!</Button>
+                                        <Button color="success" onClick={nextMem}>
+                                            I know
+                                        </Button>
+                                    </>
+                                )}
                             </ButtonGroup>
                         </Box>
                     </Stack>
                 </Box>
             </Grid>
-            <Grid item xs={2}></Grid>
+            <Grid item xs={0} md={2}></Grid>
         </Grid>
     );
 };
