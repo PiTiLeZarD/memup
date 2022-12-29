3d6e48260819d6bd759eb036d3fd2873a86061a2
Jonathan Adami <pitilezard@gmail.com>
2022-12-29 13:09:59 +1000

    Better back button that doesn't intrude on the content as much

--- src/pages/BackButton.tsx
+++ src/pages/BackButton.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { Button } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Fab } from "@mui/material";
 
 export type BackButtonProps = {};
 
@@ -14,12 +15,8 @@ export const BackButton: BackButtonComponent = (): JSX.Element => {
     if (pathname == "/") return <></>;
 
     return (
-        <Button
-            variant="contained"
-            onClick={() => navigate("/")}
-            sx={{ position: "absolute", top: "25px", left: "25px" }}
-        >
-            &lt;-- Back
-        </Button>
+        <Fab color="primary" onClick={() => navigate("/")} sx={{ position: "absolute", top: "-25px", left: "-25px" }}>
+            <ArrowBackIcon />
+        </Fab>
     );
 };
