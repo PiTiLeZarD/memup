1a5c22ac7dfccbfeb600ca6dfb51c6c008932dd5
Jonathan Adami <pitilezard@gmail.com>
2023-01-07 12:32:27 +1000

    Getting access to the logs (list of checks)

--- src/MemForm.tsx
+++ src/MemForm.tsx
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextField } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
+import ReportIcon from "@mui/icons-material/Report";
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField,
+} from "@mui/material";
 
 import { FoldersInput } from "./FoldersInput";
 import { FuriganaInput } from "./japanese/FuriganaInput";
 import { includesKanji, newMem } from "./lib";
+import { Mem } from "./Mem";
 import { MemType, useStore } from "./store";
 
 type FormState = {
@@ -37,7 +55,7 @@ export const MemForm: MemFormComponent = ({ open, onClose }): JSX.Element => {
     const { register, handleSubmit, reset, watch, setValue } = useForm<FormState>({
         defaultValues: open ? mem2Form(open) : {},
     });
-
+    const [logsOpen, setLogsOpen] = useState<boolean>(false);
     const [setFurigana, setSetFurigana] = useState<boolean>(false);
 
     const memValue = watch("mem");
@@ -51,6 +69,7 @@ export const MemForm: MemFormComponent = ({ open, onClose }): JSX.Element => {
     }, [open]);
 
     const saveMem = useStore(({ saveMem }) => saveMem);
+    const mems = useStore(({ mems }) => mems);
 
     const handleSave = ({ mem, description, hint, notes, furigana, folders }) => {
         saveMem({
@@ -67,40 +86,86 @@ export const MemForm: MemFormComponent = ({ open, onClose }): JSX.Element => {
     const handleClose = () => onClose();
 
     return (
-        <form onSubmit={handleSubmit(handleSave)}>
-            <Dialog open={!!open} onClose={handleClose}>
-                <DialogTitle>Add a mem</DialogTitle>
-                <DialogContent sx={{ minWidth: "500px" }}>
-                    <Stack spacing={2} sx={{ margin: "0.5em 0" }}>
-                        <FoldersInput watch={watch} register={register} setValue={setValue} />
-                        <TextField label="Mem" {...register("mem")} required />
-                        {hasKanji &&
-                            (setFurigana ? (
-                                <FuriganaInput
-                                    memValue={memValue}
-                                    furigana={open ? open.furigana || [] : []}
-                                    register={register}
-                                    setValue={setValue}
-                                />
-                            ) : (
-                                <Button variant="contained" onClick={() => setSetFurigana(true)}>
-                                    Set Furigana?
-                                </Button>
-                            ))}
-                        <TextField label="Description" {...register("description")} required />
-                        <TextField label="Hint" {...register("hint")} />
-                        <TextField label="Notes" {...register("notes")} multiline rows={5} />
-                    </Stack>
+        <>
+            <Dialog open={logsOpen} onClose={() => setLogsOpen(false)}>
+                <DialogTitle>
+                    <Mem mem={open as MemType} variant="h4" />
+                </DialogTitle>
+                <DialogContent>
+                    <TableContainer component={Paper}>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Date</TableCell>
+                                    <TableCell>Success</TableCell>
+                                    <TableCell>Selected</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {open &&
+                                    open.checks.map((check) => (
+                                        <TableRow key={(check.date || new Date()).toDateString()}>
+                                            <TableCell>{check.date?.toDateString()}</TableCell>
+                                            <TableCell>{check.success ? <DoneIcon /> : <ReportIcon />}</TableCell>
+                                            <TableCell>
+                                                {(check as MemQuizzAnswer).selected && (
+                                                    <Mem
+                                                        mem={
+                                                            mems.find(
+                                                                (m) => m.id == (check as MemQuizzAnswer).selected
+                                                            ) as MemType
+                                                        }
+                                                    />
+                                                )}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </DialogContent>
                 <DialogActions>
-                    <Button variant="contained" color="inherit" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="contained" onClick={handleSubmit(handleSave)}>
-                        Save
+                    <Button variant="contained" onClick={() => setLogsOpen(false)}>
+                        OK
                     </Button>
                 </DialogActions>
             </Dialog>
-        </form>
+            <form onSubmit={handleSubmit(handleSave)}>
+                <Dialog open={!!open} onClose={handleClose}>
+                    <DialogTitle>Add a mem</DialogTitle>
+                    <DialogContent sx={{ minWidth: "500px" }}>
+                        <Stack spacing={2} sx={{ margin: "0.5em 0" }}>
+                            <FoldersInput watch={watch} register={register} setValue={setValue} />
+                            <TextField label="Mem" {...register("mem")} required />
+                            {hasKanji &&
+                                (setFurigana ? (
+                                    <FuriganaInput
+                                        memValue={memValue}
+                                        furigana={open ? open.furigana || [] : []}
+                                        register={register}
+                                        setValue={setValue}
+                                    />
+                                ) : (
+                                    <Button variant="contained" onClick={() => setSetFurigana(true)}>
+                                        Set Furigana?
+                                    </Button>
+                                ))}
+                            <TextField label="Description" {...register("description")} required />
+                            <TextField label="Hint" {...register("hint")} />
+                            <TextField label="Notes" {...register("notes")} multiline rows={5} />
+                        </Stack>
+                    </DialogContent>
+                    <DialogActions>
+                        {open && open.checks && <Button onClick={() => setLogsOpen(true)}>Logs</Button>}
+                        <Button variant="contained" color="inherit" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="contained" onClick={handleSubmit(handleSave)}>
+                            Save
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </form>
+        </>
     );
 };
