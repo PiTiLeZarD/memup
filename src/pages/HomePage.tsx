720f8f7bb80a97ae9bad41343d251f908d0510de
Jonathan Adami <pitilezard@gmail.com>
2023-01-10 10:32:18 +1000

    pay heed to the warnings

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -132,67 +132,64 @@ export const HomePage: HomePageComponent = (): JSX.Element => {
                         will delete all your current mems. I will work in making it more clever in the future.
                     </Typography>
                     <Typography variant="h4">Things to keep in mind</Typography>
-                    <Typography>
-                        <ul>
-                            <li>I use the word fail but by using this tool, you never fail, you just learn.</li>
-                            <li>
-                                If you learn too many new mems at the same time, you might end up having days with a too
-                                long list to handle. It's not an issue in itself but pace yourself and play the long
-                                game.
-                            </li>
-                            <li>
-                                This tool is new, it will evolve, if you want to contribute by either providing ideas or
-                                pull requests or even money, just{" "}
-                                <a href="https://github.com/PiTiLeZarD" target="_blank">
-                                    contact me on github
-                                </a>{" "}
-                                ;)
-                            </li>
-                            <li>
-                                <b>Import is destructive</b>, it will ruin your progress and pre-existing mems. It's
-                                meant to be a backup feature but I'll update it soon.
-                            </li>
-                        </ul>
-                    </Typography>
+                    <ul>
+                        <Typography component="li">
+                            I use the word fail but by using this tool, you never fail, you just learn.
+                        </Typography>
+                        <Typography component="li">
+                            If you learn too many new mems at the same time, you might end up having days with a too
+                            long list to handle. It's not an issue in itself but pace yourself and play the long game.
+                        </Typography>
+                        <Typography component="li">
+                            This tool is new, it will evolve, if you want to contribute by either providing ideas or
+                            pull requests or even money, just{" "}
+                            <a href="https://github.com/PiTiLeZarD" target="_blank">
+                                contact me on github
+                            </a>{" "}
+                            ;)
+                        </Typography>
+                        <Typography component="li">
+                            <b>Import is destructive</b>, it will ruin your progress and pre-existing mems. It's meant
+                            to be a backup feature but I'll update it soon.
+                        </Typography>
+                    </ul>
                     <Typography variant="h4">Glossary</Typography>
-                    <Typography>
-                        A couple of definition to make things easier:
-                        <TableContainer>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Term</TableCell>
-                                        <TableCell>Definition</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell>Mem</TableCell>
-                                        <TableCell>The thing you're trying to memorise</TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Deck</TableCell>
-                                        <TableCell>A group of mems</TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Fail</TableCell>
-                                        <TableCell>The process of learning</TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Quizz</TableCell>
-                                        <TableCell>
-                                            An interface presenting you with a part of a mem, and asking you to guess
-                                            which is it in a selection within the deck
-                                        </TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Flashcard</TableCell>
-                                        <TableCell>An interface asking you simply if you remember a mem.</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Typography>
+                    <Typography>A couple of definition to make things easier:</Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Term</TableCell>
+                                    <TableCell>Definition</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Mem</TableCell>
+                                    <TableCell>The thing you're trying to memorise</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Deck</TableCell>
+                                    <TableCell>A group of mems</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Fail</TableCell>
+                                    <TableCell>The process of learning</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Quizz</TableCell>
+                                    <TableCell>
+                                        An interface presenting you with a part of a mem, and asking you to guess which
+                                        is it in a selection within the deck
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Flashcard</TableCell>
+                                    <TableCell>An interface asking you simply if you remember a mem.</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Stack>
             </ContentBox>
         </Stack>
