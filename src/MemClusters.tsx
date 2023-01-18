4e0844e367d8e9c3fd501b635a32d7bfd37de0f6
Jonathan Adami <pitilezard@gmail.com>
2023-01-19 06:27:46 +1000

    add markers so I don't have to hover over as much

--- src/MemClusters.tsx
+++ src/MemClusters.tsx
@@ -10,6 +10,15 @@ export type MemClustersProps = {
     mems: MemType[];
 };
 
+const markerStyles = {
+    position: "absolute",
+    top: "0",
+    height: "3em",
+    background: lightBlue[900],
+    transform: "translateX(-50%)",
+    zIndex: 1,
+};
+
 export type MemClustersComponent = React.FunctionComponent<MemClustersProps>;
 
 export const MemClusters: MemClustersComponent = ({ mems }): JSX.Element => {
@@ -32,18 +41,6 @@ export const MemClusters: MemClustersComponent = ({ mems }): JSX.Element => {
         <Stack spacing={2}>
             <Typography>Mems will be available as follows: (upcoming 2 days)</Typography>
             <Box sx={{ position: "relative", width: "100%", height: "3em" }}>
-                <Box
-                    sx={{
-                        position: "absolute",
-                        width: "100%",
-                        height: "0.5em",
-                        background: lightBlue[200],
-                        top: "50%",
-                        left: 0,
-                        marginRight: "2em",
-                        transform: "translateY(-50%)",
-                    }}
-                />
                 {clusters
                     .map((c) => [memScore(c[c.length - 1]).nextCheck, c.length])
                     .map(([nextCheck, count], i) => (
@@ -64,6 +61,76 @@ export const MemClusters: MemClustersComponent = ({ mems }): JSX.Element => {
                             <Chip label={String(count)} />
                         </Tooltip>
                     ))}
+                <Box
+                    sx={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "0.5em",
+                        background: lightBlue[200],
+                        top: "50%",
+                        left: 0,
+                        marginRight: "2em",
+                        transform: "translateY(-50%)",
+                        zIndex: 1,
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "50%",
+                        width: "4px",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "25%",
+                        width: "2px",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "75%",
+                        width: "2px",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "12.5%",
+                        width: "1px",
+                        height: "2em",
+                        top: "0.5em",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "37.5%",
+                        width: "1px",
+                        height: "2em",
+                        top: "0.5em",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "62.5%",
+                        width: "1px",
+                        height: "2em",
+                        top: "0.5em",
+                    }}
+                />
+                <Box
+                    sx={{
+                        ...markerStyles,
+                        left: "87.5%",
+                        width: "1px",
+                        height: "2em",
+                        top: "0.5em",
+                    }}
+                />
             </Box>
         </Stack>
     );
