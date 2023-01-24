7f6d7e5f379daac07d59a7441a1adf1b50792f66
Jonathan Adami <pitilezard@gmail.com>
2023-01-25 09:19:12 +1000

    import with conflicts step 3) beginning of the conflict interface

--- src/pages/ConflictsPage.tsx
+++ src/pages/ConflictsPage.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+
+import PreviewIcon from "@mui/icons-material/Preview";
+import { Button, Grid, IconButton, List, ListItem, Stack } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+import { MemListItem } from "../MemListItem";
+import { MemType, useStore } from "../store";
 import { HomeButton } from "./buttons/HomeButton";
 import { ContentBox } from "./ContentBox";
 
@@ -7,10 +14,42 @@ export type ConflictsPageProps = {};
 export type ConflictsPageComponent = React.FunctionComponent<ConflictsPageProps>;
 
 export const ConflictsPage: ConflictsPageComponent = (): JSX.Element => {
+    const conflicts = useStore(({ conflicts }) => conflicts);
+    const setConflicts = useStore(({ setConflicts }) => setConflicts);
+    const navigate = useNavigate();
+
+    const handlePreview = (mem: MemType) => () => {};
+    const handleClearAll = () => {
+        if (confirm("Are you sure to delete all current conflicts?")) {
+            setConflicts([]);
+            navigate("/");
+        }
+    };
+
     return (
         <ContentBox>
             <HomeButton />
-            Conflicts here
+            <Stack spacing={2}>
+                <Button variant="contained" onClick={handleClearAll}>
+                    Clear all
+                </Button>
+                <List>
+                    {conflicts.map((mem) => (
+                        <ListItem key={mem.id}>
+                            <Grid container>
+                                <Grid item xs={10}>
+                                    <MemListItem data={mem} />
+                                </Grid>
+                                <Grid item xs={2} sx={{ textAlign: "right" }}>
+                                    <IconButton onClick={handlePreview(mem)}>
+                                        <PreviewIcon />
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                    ))}
+                </List>
+            </Stack>
         </ContentBox>
     );
 };
