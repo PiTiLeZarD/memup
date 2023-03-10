2ff4c503b3cd420bfc973bff6f7806bb5e913b59
Jonathan Adami <pitilezard@gmail.com>
2023-03-11 06:50:43 +1000

    Edit button on the DeckBrowser

--- src/modes/DeckBrowser.tsx
+++ src/modes/DeckBrowser.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useCountdown } from "usehooks-ts";
 
+import EditIcon from "@mui/icons-material/Edit";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import MovingIcon from "@mui/icons-material/Moving";
 import TrendingDownIcon from "@mui/icons-material/TrendingDown";
-import { Alert, Box, Button, Divider, Grid, Stack, Tooltip, Typography } from "@mui/material";
+import { Alert, Box, Button, Divider, Grid, IconButton, Stack, Tooltip, Typography } from "@mui/material";
 
 import { useMemo } from "react";
 import { levelGapMap, memScore } from "../lib";
 import { Mem } from "../Mem";
+import { MemFormDialog } from "../MemFormDialog";
 import { MemAnswer, MemType, useStore } from "../store";
 import { FlashCard } from "./FlashCard";
 import { Quizz } from "./Quizz";
@@ -22,6 +24,8 @@ export type DeckBrowserComponent = React.FunctionComponent<DeckBrowserProps>;
 
 export const DeckBrowser: DeckBrowserComponent = ({ mems }): JSX.Element => {
     const [currentMem, setCurrentMem] = useState<number>(0);
+    const [formOpen, setFormOpen] = useState<false | MemType>(false);
+
     const mem = mems[currentMem] || null;
     const score = useMemo(() => (mem ? memScore(mem) : null), [mem?.id]);
 
@@ -63,69 +67,84 @@ export const DeckBrowser: DeckBrowserComponent = ({ mems }): JSX.Element => {
     };
 
     return (
-        <Grid container>
-            <Grid item xs={0} md={2}></Grid>
-            <Grid item xs={12} md={8}>
-                <Box>
-                    <Stack spacing={2} sx={{ textAlign: "center" }}>
-                        <Stack direction="row" sx={{ width: "auto", margin: "auto" }}>
-                            <Alert severity="success" icon={<MovingIcon />}>
-                                {scores.up}
-                            </Alert>
-                            <Typography variant="h5" sx={{ padding: "6px 2em" }}>
-                                {mem ? (
-                                    <>
-                                        {currentMem + 1} / {mems.length}
-                                    </>
-                                ) : (
-                                    "Done!"
-                                )}
-                            </Typography>
-                            <Alert severity="error" icon={<TrendingDownIcon />}>
-                                {scores.down}
-                            </Alert>
-                        </Stack>
+        <>
+            {" "}
+            <MemFormDialog open={formOpen} setOpen={setFormOpen} />
+            <Grid container>
+                <Grid item xs={0} md={2}></Grid>
+                <Grid item xs={12} md={8}>
+                    <Box>
+                        <Stack spacing={2} sx={{ textAlign: "center" }}>
+                            <Stack direction="row" sx={{ width: "auto", margin: "auto" }}>
+                                <Alert severity="success" icon={<MovingIcon />}>
+                                    {scores.up}
+                                </Alert>
+                                <Typography variant="h5" sx={{ padding: "6px 2em" }}>
+                                    {mem ? (
+                                        <>
+                                            {currentMem + 1} / {mems.length}
+                                        </>
+                                    ) : (
+                                        "Done!"
+                                    )}
+                                </Typography>
+                                <Alert severity="error" icon={<TrendingDownIcon />}>
+                                    {scores.down}
+                                </Alert>
+                            </Stack>
 
-                        {mem && score && (
-                            <>
-                                {!titleDescription && (
-                                    <Stack direction="row" spacing={4} justifyContent="center">
-                                        <Mem mem={mem} variant="h2" sx={{ paddingTop: "0.5em" }} />
-                                        {mem.hint ? (
-                                            <Tooltip title={mem.hint}>
-                                                <HelpOutlineIcon />
-                                            </Tooltip>
-                                        ) : (
-                                            <></>
-                                        )}
+                            {mem && score && (
+                                <>
+                                    {!titleDescription && (
+                                        <Stack direction="row" spacing={4} justifyContent="center">
+                                            <Mem mem={mem} variant="h2" sx={{ paddingTop: "0.5em" }} />
+                                            {mem.hint ? (
+                                                <Tooltip title={mem.hint}>
+                                                    <HelpOutlineIcon />
+                                                </Tooltip>
+                                            ) : (
+                                                <></>
+                                            )}
+                                        </Stack>
+                                    )}
+                                    {titleDescription && <Typography variant="h2">{mem.description}</Typography>}
+
+                                    {inLevels && <Timer time={time} maxTime={maxTime} />}
+
+                                    {inLevels ? (
+                                        <Quizz
+                                            answer={handleAnswer}
+                                            mem={mem}
+                                            timesup={time == 0}
+                                            memory={score.memory}
+                                        />
+                                    ) : (
+                                        <FlashCard answer={handleAnswer} mem={mem} />
+                                    )}
+                                </>
+                            )}
+
+                            {!mem && <Typography variant="h3">Congrats, that was a nice sesh!</Typography>}
+
+                            {currentScore !== null && (
+                                <>
+                                    <Divider />
+                                    <Stack direction="row">
+                                        <IconButton onClick={() => setFormOpen(mem)}>
+                                            <EditIcon />
+                                        </IconButton>
+
+                                        <Button variant="contained" onClick={handleNextMem} fullWidth>
+                                            Next
+                                        </Button>
                                     </Stack>
-                                )}
-                                {titleDescription && <Typography variant="h2">{mem.description}</Typography>}
-
-                                {inLevels && <Timer time={time} maxTime={maxTime} />}
-
-                                {inLevels ? (
-                                    <Quizz answer={handleAnswer} mem={mem} timesup={time == 0} memory={score.memory} />
-                                ) : (
-                                    <FlashCard answer={handleAnswer} mem={mem} />
-                                )}
-                            </>
-                        )}
-
-                        {!mem && <Typography variant="h3">Congrats, that was a nice sesh!</Typography>}
-
-                        {currentScore !== null && (
-                            <>
-                                <Divider />
-                                <Button variant="contained" onClick={handleNextMem}>
-                                    Next
-                                </Button>
-                            </>
-                        )}
-                    </Stack>
-                </Box>
+                                </>
+                            )}
+                        </Stack>
+                    </Box>
+                </Grid>
+                <Grid item xs={0} md={2}></Grid>
             </Grid>
-            <Grid item xs={0} md={2}></Grid>
-        </Grid>
+        </>
     );
 };
